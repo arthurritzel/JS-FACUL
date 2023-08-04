89c52bc19a9dfd41658ce1b324ec57a70aeac984
Arthur Burkhard Ritzel <arthurritzel@MacBook-Air-de-Arthur.local>
2023-08-04 20:18:56 -0300

    lista js completo

--- lista002/exe011.js
+++ lista002/exe011.js
@@ -11,13 +11,22 @@ var conta = {
         console.log("Seu saldo atual é %f senhor(a) %s", this.saldo, this.nomeTitu)
     },
     saque: function(){
-        this.saldo -= parseFloat(prompt("Digite o valor de saque"));
+        do{
+            var saqueaux = parseFloat(prompt("Digite o valor de saque"));
+            if(saqueaux > this.saldo){
+                console.log("Voce nao possue esse saldo!");
+                flag = 1;
+            }else{
+                this.saldo -= saqueaux;
+                flag = 0;
+            }
+        }while(flag != 0);
         console.log("Seu saldo atual é %f senhor(a) %s", this.saldo, this.nomeTitu)
     }
 }
 
 do{
-    var esc = prompt("Digite a operacao que deseja\n0-cancelar\n1- deposito\n2- Saque")
+    var esc = prompt("Digite a operacao que deseja realizar\nConta "+ conta.numero +" \nagencia: "+conta.agencia+" \n0-cancelar\n1- deposito\n2- Saque");
     if (esc == 1) {
         conta.deposito();
     }else if(esc == 2){
