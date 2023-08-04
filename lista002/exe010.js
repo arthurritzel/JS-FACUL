907017efa615ecafb6c2af0aa6f591b5ef9e6266
Arthur Burkhard Ritzel <arthurritzel@MacBook-Air-de-Arthur.local>
2023-08-03 21:44:49 -0300

    lista js completo

--- lista002/exe010.js
+++ lista002/exe010.js
@@ -1,15 +1,15 @@
 var calc = {
     adicao: function(n1, n2){
-        return n1+n2
+        return n1+n2;
     },
     subtracao: function(n1, n2){
-        return n1-n2
+        return n1-n2;
     },
     divisao: function(n1, n2){
-        return n1/n2
+        return n1/n2;
     },
     multiplicacao: function(n1, n2){
-        return n1*n2
+        return n1*n2;
     },
 }
 var esc = prompt("Qual tipo de operacao deseja realizar\n1-adicao\n2-subtracao\n3-divisao\n4-multiplicação")
