e9c674bd8a825a9a9dc33b2a6adec4e1f64448ad
Arthur Burkhard Ritzel <arthurritzel@MacBook-Air-de-Arthur.local>
2023-09-08 20:07:24 -0300

    exe004 correction

--- lista006/exe004.js
+++ lista006/exe004.js
@@ -1,5 +1,18 @@
-console.log(navigator.userAgent)
-var p = document.createElement("p")
-
-p.innerHTML = navigator.userAgent
-document.body.appendChild(p)
+var sBrowser,
+  sUsrAg = navigator.userAgent;
+console.log (sUsrAg)
+if (sUsrAg.indexOf("Chrome") > -1) {
+  sBrowser = "Google Chrome";
+} else if (sUsrAg.indexOf("Safari") > -1) {
+  sBrowser = "Apple Safari";
+} else if (sUsrAg.indexOf("Opera") > -1) {
+  sBrowser = "Opera";
+} else if (sUsrAg.indexOf("Firefox") > -1) {
+  sBrowser = "Mozilla Firefox";
+} else if (sUsrAg.indexOf("MSIE") > -1) {
+  sBrowser = "Microsoft Internet Explorer";
+}
+var h1 = document.createElement("h1")
+h1.innerHTML = sBrowser
+h1.style.textAlign = "center"
+document.body.appendChild(h1)
